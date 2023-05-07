38b22b08b00652e086437db00b17ccf177a9bcf5
Giovanne Ferreira <giovannefd@gmail.com>
2023-05-07 18:00:25 -0300

    aula sobre terminais e argumentos, executar fora da ide

--- terminais e argumentos/src/MinhaClasse.java
+++ terminais e argumentos/src/MinhaClasse.java
@@ -1,8 +1,11 @@
+import java.security.PublicKey;
 
 public class MinhaClasse {
 
-	public static void main(String[] args) {
-		System.out.println("Oi, fui executado pelo terminal !!!");
-	}
+	public static void main(String[] args) throws Exception {
+	      int numero1 = 1;
+	      String numero2 = "2";
+	      System.out.println(numero1+numero2); 
+	    }
 
 }
